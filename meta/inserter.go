ab068ed5e89b493763c8d56b1ce8e90eb5ef6447
Fan Yang <fanyang@apecloud.com>
2024-09-10 11:54:22 +0800

    Save work

---
+++ meta/inserter.go
@@ -0,0 +1,33 @@
+package meta
+
+import (
+	stdsql "database/sql"
+
+	"github.com/dolthub/go-mysql-server/sql"
+)
+
+type rowInserter struct {
+	tempTableName string
+	conn          *stdsql.Conn
+}
+
+var _ sql.RowInserter = &rowInserter{}
+
+func (ri *rowInserter) StatementBegin(ctx *sql.Context) {
+	ctx.ID()
+}
+
+func (ri *rowInserter) DiscardChanges(ctx *sql.Context, errorEncountered error) error {
+
+}
+
+func (ri *rowInserter) StatementComplete(ctx *sql.Context) error {
+	return nil
+}
+
+func (ri *rowInserter) Close(ctx *sql.Context) error {
+
+}
+
+func (ri *rowInserter) Insert(ctx *sql.Context, row sql.Row) error {
+}
